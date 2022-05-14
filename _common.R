e5c581cce0f98c89bb8d2c617d859d56ef311351
Hongyuan Jia <hongyuanjia@cqust.edu.cn>
2022-05-14 19:08:32 +0800

    Consistent formatting for Rmd files (#12)

--- _common.R
+++ _common.R
@@ -3,16 +3,20 @@ hook_output <- knitr::knit_hooks$get("output")
 
 # set a new output hook to truncate text output
 knitr::knit_hooks$set(output = function(x, options) {
-  if (!is.null(n <- options$out.lines)) {
-    x <- xfun::split_lines(x)
-    if (length(x) > n) {
-      # truncate the output
-      x <- c(head(x, n), "....\n")
+    if (!is.null(n <- options$out.lines)) {
+        x <- xfun::split_lines(x)
+        if (length(x) > n) {
+            # truncate the output
+            x <- c(head(x, n), "....\n")
+        }
+        x <- paste(x, collapse = "\n")
     }
-    x <- paste(x, collapse = "\n")
-  }
-  hook_output(x, options)
+    hook_output(x, options)
 })
 
 # set default output lines to 10
-knitr::opts_chunk$set(echo = TRUE, collapse = TRUE, tidy = TRUE, out.lines = 10)
+knitr::opts_chunk$set(
+    echo = TRUE, collapse = TRUE,
+    tidy = "styler", tidy.opts = list(indent_by = 4),
+    out.lines = 10
+)
